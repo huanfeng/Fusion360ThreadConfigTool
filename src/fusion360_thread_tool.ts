14a3d1bd25ed229f0b3671cb577427181dfe8102
Dufeng <dufeng@aijiasmart.com>
2023-04-04 20:30:23 +0800

    feat: add fusion360_thread_tool.ts

---
+++ src/fusion360_thread_tool.ts
@@ -0,0 +1,12 @@
+type XmlConfig = {
+  offsets: number[]
+  handleInternel: true
+  handleExternal: true
+  reserveOriginal: true
+}
+
+function generalXml(input: string, cfg: XmlConfig): string {
+  return ''
+}
+
+export { generalXml }
